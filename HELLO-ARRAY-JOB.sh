5fb4df632fd83f4b20a328591796147a135cd1c6
root <root@70a6702a41fe>
2018-02-11 12:19:14 +0000

    .

--- HELLO-ARRAY-JOB.sh
+++ HELLO-ARRAY-JOB.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=HELLO
 ##SBATCH --workdir=/home/root
 #SBATCH --ntasks-per-node=1
-#SBATCH --array=1-10
+#SBATCH --array=1-5000
 #SBATCH --nodes=1
 #SBATCH --time=100:00
-#SBATCH --output=slurm-%A_%a.log
+#SBATCH --output=SLURM-%A_%a.LOG
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jehoon.song@standigm.com
 
@@ -27,8 +27,8 @@ echo "MATCH p=(x:Compound)-[*2]-(y:Disease) return x.name, y.name limit 1000;" |
 
 # Post Processing. 
 HASH=`md5sum ${OUTPUT} | awk '{ print $1 }'`
-#curl --user "tableshare:sait2012$" -X MKCOL "http://192.168.0.89:5005/homes/tableshare/archive/${HASH}"
-#curl --user "tableshare:sait2012$" -T "${TEMP_WD}/${OUTPUT}" "http://192.168.0.89:5005/homes/tableshare/archive/${HASH}/"
+#curl --user tableshare:sait2012$ -X MKCOL http://192.168.0.89:5005/homes/tableshare/archive/${HASH}
+#curl --user tableshare:sait2012$ -T ${TEMP_WD}/${OUTPUT} http://192.168.0.89:5005/homes/tableshare/archive/${HASH}/
 UPLOAD_DIR="http://192.168.0.89:5005/homes/tableshare/slurm/${JOB_ID}"
 curl --user "tableshare:sait2012$" -X MKCOL ${UPLOAD_DIR}
 curl --user "tableshare:sait2012$" -T ${TEMP_WD}/${OUTPUT} ${UPLOAD_DIR}/
